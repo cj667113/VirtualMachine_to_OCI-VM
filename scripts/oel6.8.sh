c61e626475236b0a003494328530889e00dce372
Chris Johnston <40300884+cj667113@users.noreply.github.com>
2024-03-12 08:15:35 -0700

    Update oel6.8.sh

--- scripts/oel6.8.sh
+++ scripts/oel6.8.sh
@@ -10,8 +10,18 @@ echo 'add_dracutmodules+="iscsi"' >> /etc/dracut.conf
 echo "ISCSI Modules Added to Dracut"
 sudo sed -i '/^timeout=/ s/=.*/=2/' /boot/grub/grub.conf
 sudo sed -i '/^splashimage/ s/^/#/' /boot/grub/grub.conf
-sudo sed -i 's/\(kernel.*\)/\1 console=ttyS0,9600/' /boot/grub/grub.conf
+sudo sed -i 's/\(kernel.*\)/\1 ip=dhcp console=ttyS0,9600/' /boot/grub/grub.conf
 echo "Grub Config Updated"
+echo "Scrubbed Hard Codded MAC"
+for file in /etc/sysconfig/network-scripts/ifcfg-*; do
+    # Check if the file exists and is a regular file
+    if [ -f "$file" ]; then
+        # Remove or comment out the HWADDR line
+        sudo sed -i '/^HWADDR=/ s/^/#/' "$file"
+        echo "Hardcoded MAC removed from $file"
+    fi
+done
+echo "Scrubbed Hard Codded MAC"
 sudo stty -F /dev/ttyS0 speed 9600
 dmesg | grep console
 echo "Executing Dracut"
