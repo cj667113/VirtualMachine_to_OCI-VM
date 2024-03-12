66e3e879270ee622d81fe014b87bd883fa0c5014
Chris Johnston <40300884+cj667113@users.noreply.github.com>
2024-03-11 19:32:52 -0700

    Update oel6.8.sh
    BETA - Converted for BIOS Boot

--- scripts/oel6.8.sh
+++ scripts/oel6.8.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
-echo "This is currently in BETA"
 echo "Developed by Christopher M Johnston"
-echo "03/11/2024"
-echo "Configures OEL 6.8 to be moved to OCI Bare Metal Infrastructure"
+echo "05/23/2020"
+echo "Configures RHEL 6.8 to be moved to OCI Bare Metal Infrastructure"
 sudo ln -sf /etc/systemd/system/multi-user.target /etc/systemd/system/default.target
 sudo yum install dracut-network iscsi-initiator-utils -y
 echo "Dependencies Installed"
 echo 'add_dracutmodules+="iscsi"' >> /etc/dracut.conf
 echo "ISCSI Modules Added to Dracut"
-cat /boot/grub/grub.conf | grep -v 'console=ttyS0' > /tmp/grub
-echo 'serial --unit=0 --speed=9600 --word=8 --parity=no --stop=1' >> /tmp/grub
-echo 'terminal --timeout=5 serial console' >> /tmp/grub
-sed -i 's/^timeout=.*/timeout=5/' /tmp/grub
-cp /tmp/grub /boot/grub/grub.conf
-echo "Grub Config Made"
+sudo sed -i '/^timeout=/ s/=.*/=5/' /boot/grub/grub.conf
+sudo sed -i '/^splashimage/ s/^/#/' /boot/grub/grub.conf
+sudo sed -i '1i serial --unit=0 --speed=9600 --word=8 --parity=no --stop=1\nterminal --timeout=5 serial console' /boot/grub/grub.conf
+sudo sed -i '/^kernel/ s/$/ console=ttyS0,9600 netroot=iscsi:@169.254.0.2::::iqn.2015-02.oracle.boot:uefi iscsi_initiator=iqn.2015-02.oracle.boot:instance/' /boot/grub/grub.conf
+echo "Grub Config Updated"
 sudo stty -F /dev/ttyS0 speed 9600
 dmesg | grep console
 sudo chkconfig --level 2345 serial on
 sudo service serial start
 echo "Executing Dracut"
 for file in $(find /boot -name "vmlinuz-*" -and -not -name "vmlinuz-*rescue*") ; do
-    dracut --force --no-hostonly /boot/initramfs-${file:14}.img ${file:14}
+    sudo dracut --force --no-hostonly /boot/initramfs-${file:14}.img ${file:14}
 done
 echo "Dracut Executed"
 echo "Shutting Down"
